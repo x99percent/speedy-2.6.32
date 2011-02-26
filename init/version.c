d0db26c7c9cf13ce007cac7426b5a797f053bdf4
x99percent <tony@99percent.com>
2011-02-26 12:26:20 -0700

    Name kernel

--- init/version.c
+++ init/version.c
@@ -43,5 +43,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (x99percent)"
 	" (" LINUX_COMPILER ") %s\n";
